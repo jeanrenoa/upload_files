6cb645e7c8dfb6624b0582e1b41de4b9a41e30a7
jeanrenoa <jeanrenoa@hotmail.com>
2013-09-30 14:29:41 +0800

    update the file path to network location
    Need to use //xxxx/xxxx for network folder

--- find-all-files.py
+++ find-all-files.py
@@ -1,8 +1,8 @@
 import fnmatch
 import os
   
-rootPath = '.'
-pattern = '*.txt'
+rootPath = '//panda.autodesk.com/BRE_MASTERS_PSEB/ADSK_Materials/Longbow/px86x64'
+pattern = '*.zip'
   
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
